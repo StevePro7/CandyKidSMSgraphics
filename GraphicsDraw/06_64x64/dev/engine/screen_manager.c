d660d90776cb0bc594129ccbbf5a581bae8e6abc
StevePro7 <steveprozune@hotmail.com>
2019-12-17 21:10:53 +0000

    no message

---
+++ GraphicsDraw/06_64x64/dev/engine/screen_manager.c
@@ -0,0 +1,13 @@
+#include "screen_manager.h"
+#include "font_manager.h"
+
+#define NORMAL_TILES_TEXT	"NORMAL TILES LOADED..."
+#define SPRITE_TILES_TEXT	"SPRITE TILES LOADED..."
+
+void engine_screen_manager_init()
+{
+}
+
+void engine_screen_manager_update()
+{
+}
