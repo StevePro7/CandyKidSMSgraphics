9806fd49ad907e0c7e9e7ec39aeb606217f5d789
StevePro7 <steveprozune@hotmail.com>
2019-12-17 21:19:13 +0000

    no message

--- GraphicsDraw/06_64x64/dev/engine/screen_manager.asm
+++ GraphicsDraw/06_64x64/dev/engine/screen_manager.asm
@@ -8,6 +8,8 @@
 ;--------------------------------------------------------
 ; Public variables in this module
 ;--------------------------------------------------------
+	.globl _devkit_SMS_addSprite
+	.globl _engine_font_manager_draw_text
 	.globl _engine_screen_manager_init
 	.globl _engine_screen_manager_update
 ;--------------------------------------------------------
@@ -41,19 +43,83 @@
 ; code
 ;--------------------------------------------------------
 	.area _CODE
-;screen_manager.c:7: void engine_screen_manager_init()
+;screen_manager.c:8: void engine_screen_manager_init()
 ;	---------------------------------
 ; Function engine_screen_manager_init
 ; ---------------------------------
 _engine_screen_manager_init::
-;screen_manager.c:9: }
+;screen_manager.c:10: engine_font_manager_draw_text( SPRITE_TILES_TEXT, 2, 0 );
+	ld	hl, #0x0002
+	push	hl
+	ld	hl, #___str_0
+	push	hl
+	call	_engine_font_manager_draw_text
+	pop	af
+	pop	af
 	ret
-;screen_manager.c:11: void engine_screen_manager_update()
+___str_0:
+	.ascii "SPRITE TILES LOADED..."
+	.db 0x00
+;screen_manager.c:13: void engine_screen_manager_update()
 ;	---------------------------------
 ; Function engine_screen_manager_update
 ; ---------------------------------
 _engine_screen_manager_update::
-;screen_manager.c:13: }
+	push	ix
+	ld	ix,#0
+	add	ix,sp
+	dec	sp
+;screen_manager.c:22: for( r = 0; r < 8; r++ )
+	ld	c, #0x00
+;screen_manager.c:24: for( c = 0; c < 8; c++ )
+00109$:
+	ld	a, c
+	rlca
+	rlca
+	rlca
+	and	a, #0xf8
+	add	a, #0x20
+	ld	-1 (ix), a
+	ld	b, #0x00
+00103$:
+;screen_manager.c:26: devkit_SMS_addSprite( x + c * 8, y + r * 8, tile + ( r * 8 + c ) );
+	ld	l, c
+	ld	h, #0x00
+	add	hl, hl
+	add	hl, hl
+	add	hl, hl
+	ld	e, b
+	ld	d, #0x00
+	add	hl, de
+	ld	de, #0x0100
+	add	hl, de
+	ld	a, b
+	rlca
+	rlca
+	rlca
+	and	a, #0xf8
+	add	a, #0x20
+	ld	e, a
+	push	bc
+	push	hl
+	ld	d, -1 (ix)
+	push	de
+	call	_devkit_SMS_addSprite
+	pop	af
+	pop	af
+	pop	bc
+;screen_manager.c:24: for( c = 0; c < 8; c++ )
+	inc	b
+	ld	a, b
+	sub	a, #0x08
+	jr	C,00103$
+;screen_manager.c:22: for( r = 0; r < 8; r++ )
+	inc	c
+	ld	a, c
+	sub	a, #0x08
+	jr	C,00109$
+	inc	sp
+	pop	ix
 	ret
 	.area _CODE
 	.area _INITIALIZER
--- GraphicsDraw/06_64x64/dev/engine/screen_manager.c
+++ GraphicsDraw/06_64x64/dev/engine/screen_manager.c
@@ -1,13 +1,30 @@
 #include "screen_manager.h"
 #include "font_manager.h"
+#include "..\devkit\_sms_manager.h"
 
 #define NORMAL_TILES_TEXT	"NORMAL TILES LOADED..."
 #define SPRITE_TILES_TEXT	"SPRITE TILES LOADED..."
 
 void engine_screen_manager_init()
 {
+	engine_font_manager_draw_text( SPRITE_TILES_TEXT, 2, 0 );
 }
 
 void engine_screen_manager_update()
 {
+	unsigned char x = 32;
+	unsigned char y = 32;
+	unsigned int tile = 256;
+
+	unsigned char r = 0;
+	unsigned char c = 0;
+	
+	for( r = 0; r < 8; r++ )
+	{
+		for( c = 0; c < 8; c++ )
+		{
+			devkit_SMS_addSprite( x + c * 8, y + r * 8, tile + ( r * 8 + c ) );
+		}
+	}
+	
 }
