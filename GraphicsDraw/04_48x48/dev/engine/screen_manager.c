91c1cde5ba47c9c78e18fad788974c6c70890889
StevePro7 <steveprozune@hotmail.com>
2019-12-17 21:34:38 +0000

    no message

--- GraphicsDraw/04_48x48/dev/engine/screen_manager.c
+++ GraphicsDraw/04_48x48/dev/engine/screen_manager.c
@@ -1,5 +1,6 @@
 #include "screen_manager.h"
 #include "font_manager.h"
+#include "..\devkit\_sms_manager.h"
 
 #define NORMAL_TILES_TEXT	"NORMAL TILES LOADED..."
 #define SPRITE_TILES_TEXT	"SPRITE TILES LOADED..."
@@ -10,4 +11,19 @@ void engine_screen_manager_init()
 
 void engine_screen_manager_update()
 {
+	const unsigned char max = 6;
+	unsigned char x = 32;
+	unsigned char y = 32;
+	unsigned int tile = 256;
+
+	unsigned char r = 0;
+	unsigned char c = 0;
+
+	for( r = 0; r < max; r++ )
+	{
+		for( c = 0; c < max; c++ )
+		{
+			devkit_SMS_addSprite( x + c * max, y + r * max, tile + ( r * max + c ) );
+		}
+	}
 }
--- GraphicsDraw/readme.md
+++ GraphicsDraw/readme.md
@@ -9,3 +9,17 @@
 36x tiles
 6x  high
 6x  wide
+
+64x64
+64x tiles
+8x  wide
+8x  high
+
+But don't think will bother with 64x64 boss
+as could not render 64x64 boss AND CK on same plane
+
+
+Finally, if wanted to have 2x 48x48 bosses then budget is:
+36x tiles per boss * 2 animations =  72x tiles
+double this for 2x bosses         = 144x tiles
+Need CK = 8 + 4 for death
