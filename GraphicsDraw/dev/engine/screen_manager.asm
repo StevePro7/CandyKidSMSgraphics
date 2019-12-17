b3b073fea4cf0b734ba343f3e3e7d11edd23d0f3
StevePro7 <steveprozune@hotmail.com>
2019-12-17 20:32:00 +0000

    no message

---
+++ GraphicsDraw/dev/engine/screen_manager.asm
@@ -0,0 +1,60 @@
+;--------------------------------------------------------
+; File Created by SDCC : free open source ANSI-C Compiler
+; Version 3.6.0 #9615 (MINGW64)
+;--------------------------------------------------------
+	.module screen_manager
+	.optsdcc -mz80
+	
+;--------------------------------------------------------
+; Public variables in this module
+;--------------------------------------------------------
+	.globl _engine_screen_manager_init
+	.globl _engine_screen_manager_update
+;--------------------------------------------------------
+; special function registers
+;--------------------------------------------------------
+;--------------------------------------------------------
+; ram data
+;--------------------------------------------------------
+	.area _DATA
+;--------------------------------------------------------
+; ram data
+;--------------------------------------------------------
+	.area _INITIALIZED
+;--------------------------------------------------------
+; absolute external ram data
+;--------------------------------------------------------
+	.area _DABS (ABS)
+;--------------------------------------------------------
+; global & static initialisations
+;--------------------------------------------------------
+	.area _HOME
+	.area _GSINIT
+	.area _GSFINAL
+	.area _GSINIT
+;--------------------------------------------------------
+; Home
+;--------------------------------------------------------
+	.area _HOME
+	.area _HOME
+;--------------------------------------------------------
+; code
+;--------------------------------------------------------
+	.area _CODE
+;screen_manager.c:7: void engine_screen_manager_init()
+;	---------------------------------
+; Function engine_screen_manager_init
+; ---------------------------------
+_engine_screen_manager_init::
+;screen_manager.c:9: }
+	ret
+;screen_manager.c:11: void engine_screen_manager_update()
+;	---------------------------------
+; Function engine_screen_manager_update
+; ---------------------------------
+_engine_screen_manager_update::
+;screen_manager.c:13: }
+	ret
+	.area _CODE
+	.area _INITIALIZER
+	.area _CABS (ABS)
