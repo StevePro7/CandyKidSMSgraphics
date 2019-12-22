d3e06388f9019e5bf5dd5239a8acf9246797f52e
StevePro7 <steveprozune@hotmail.com>
2019-12-22 16:33:50 +0000

    no message

---
+++ GraphicsDraw/11_level/dev/engine/screen_manager.c
@@ -0,0 +1,63 @@
+#include "screen_manager.h"
+#include "font_manager.h"
+#include "..\devkit\_sms_manager.h"
+#include "..\gfx.h"
+
+#define NORMAL_TILES_TEXT	"NORMAL TILES LOADED..."
+#define SPRITE_TILES_TEXT	"SPRITE TILES LOADED..."
+
+static void draw_titile();
+static void draw_tree( unsigned char x, unsigned char y );
+
+void engine_screen_manager_init()
+{
+	unsigned char x;
+	unsigned char y;
+	for( x = 2; x < 32; x += 2 )
+	{
+		draw_tree( x, 0 );
+		draw_tree( x, 22 );
+	}
+
+	for( y = 2; y < 22; y += 2 )
+	{
+		draw_tree( 2, y );
+		draw_tree( 30, y );
+	}
+
+	draw_titile();
+}
+
+void engine_screen_manager_update()
+{
+}
+
+static void draw_titile()
+{
+	const unsigned int *pnt = ( const unsigned int * ) game_title__tilemap__bin;
+
+	unsigned char x = 4;
+	unsigned char y = 2;
+
+	unsigned char i;
+	unsigned char j;
+
+	for( j = 0; j < 4; j++ )
+	{
+		for( i = 0; i < 26; i++ )
+		{
+			devkit_SMS_setNextTileatXY( x + i, y + j );
+			devkit_SMS_setTile( *pnt + j * 26 + i );
+		}
+	}
+}
+
+static void draw_tree( unsigned char x, unsigned char y )
+{
+	const unsigned int *pnt = ( const unsigned int * ) tree_death__tilemap__bin;
+
+	devkit_SMS_setNextTileatXY( x + 0, y + 0 ); 	devkit_SMS_setTile( *pnt + 0 );
+	devkit_SMS_setNextTileatXY( x + 1, y + 0 ); 	devkit_SMS_setTile( *pnt + 1 );
+	devkit_SMS_setNextTileatXY( x + 0, y + 1 ); 	devkit_SMS_setTile( *pnt + 2 );
+	devkit_SMS_setNextTileatXY( x + 1, y + 1 ); 	devkit_SMS_setTile( *pnt + 3 );
+}
---
+++ GraphicsDraw/11_level/dev/gfx.h
@@ -0,0 +1,48 @@
+extern const unsigned char	adriana__palette__bin[];
+#define				adriana__palette__bin_size 16
+
+extern const unsigned char	adriana__tiles__psgcompr[];
+#define				adriana__tiles__psgcompr_size 62
+
+extern const unsigned char	font_tiles__palette__bin[];
+#define				font_tiles__palette__bin_size 16
+
+extern const unsigned char	font_tiles__tilemap__bin[];
+#define				font_tiles__tilemap__bin_size 128
+
+extern const unsigned char	font_tiles__tiles__psgcompr[];
+#define				font_tiles__tiles__psgcompr_size 663
+
+extern const unsigned char	game_title__palette__bin[];
+#define				game_title__palette__bin_size 16
+
+extern const unsigned char	game_title__tilemap__bin[];
+#define				game_title__tilemap__bin_size 208
+
+extern const unsigned char	game_title__tiles__psgcompr[];
+#define				game_title__tiles__psgcompr_size 1995
+
+extern const unsigned char	sprites__palette__bin[];
+#define				sprites__palette__bin_size 16
+
+extern const unsigned char	sprites__tiles__psgcompr[];
+#define				sprites__tiles__psgcompr_size 998
+
+extern const unsigned char	tree_avoid__palette__bin[];
+#define				tree_avoid__palette__bin_size 16
+
+extern const unsigned char	tree_avoid__tilemap__bin[];
+#define				tree_avoid__tilemap__bin_size 8
+
+extern const unsigned char	tree_avoid__tiles__psgcompr[];
+#define				tree_avoid__tiles__psgcompr_size 82
+
+extern const unsigned char	tree_death__palette__bin[];
+#define				tree_death__palette__bin_size 16
+
+extern const unsigned char	tree_death__tilemap__bin[];
+#define				tree_death__tilemap__bin_size 8
+
+extern const unsigned char	tree_death__tiles__psgcompr[];
+#define				tree_death__tiles__psgcompr_size 115
+
