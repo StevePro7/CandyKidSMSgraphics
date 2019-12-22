2725c6168e49bc11b72adac3632b2e6631b6d158
StevePro7 <steveprozune@hotmail.com>
2019-12-22 16:57:09 +0000

    no message

--- GraphicsDraw/11_level/dev/engine/screen_manager.c
+++ GraphicsDraw/11_level/dev/engine/screen_manager.c
@@ -10,10 +10,41 @@
 #include "..\screen\test_screen.h"
 #include "..\screen\func_screen.h"
 
+static void( *load_method[ MAX_SCREEENS ] )( );
+static void( *update_method[ MAX_SCREEENS ] )( unsigned char *screen_type );
+
+static unsigned char curr_screen_type;
+static unsigned char next_screen_type;
+
 void engine_screen_manager_init( unsigned char open_screen_type )
 {
+	next_screen_type = open_screen_type;
+	curr_screen_type = screen_type_none;
+
+	// Set load methods.
+	load_method[ screen_type_none ] = screen_none_screen_load;
+	load_method[ screen_type_splash ] = screen_splash_screen_load;
+	load_method[ screen_type_intro ] = screen_intro_screen_load;
+	load_method[ screen_type_load ] = screen_load_screen_load;
+	load_method[ screen_type_test ] = screen_test_screen_load;
+	load_method[ screen_type_func ] = screen_func_screen_load;
+
+	// Set update methods.
+	update_method[ screen_type_none ] = screen_none_screen_update;
+	update_method[ screen_type_splash ] = screen_splash_screen_update;
+	update_method[ screen_type_intro ] = screen_intro_screen_update;
+	update_method[ screen_type_load ] = screen_load_screen_update;
+	update_method[ screen_type_test ] = screen_test_screen_update;
+	update_method[ screen_type_func ] = screen_func_screen_update;
 }
 
 void engine_screen_manager_update()
 {
+	if( curr_screen_type != next_screen_type )
+	{
+		curr_screen_type = next_screen_type;
+		load_method[ curr_screen_type ]();
+	}
+
+	update_method[ curr_screen_type ]( &next_screen_type );
 }
