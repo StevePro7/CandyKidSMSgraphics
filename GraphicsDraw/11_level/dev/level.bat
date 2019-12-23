1baecb2401d7c81186add39834ee053a5c73a167
StevePro7 <steveprozune@hotmail.com>
2019-12-23 14:06:01 +0000

    no message

--- GraphicsDraw/11_level/dev/level.bat
+++ GraphicsDraw/11_level/dev/level.bat
@@ -5,9 +5,9 @@ REM cd ..
 cd banks
 
 folder2c bank2 bank2 2
-::folder2c bank3 bank3 3
+folder2c bank3 bank3 3
 sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK2 bank2.c
-::sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK3 bank3.c
+sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK3 bank3.c
 
 del *.asm > nul
 del *.lst > nul
