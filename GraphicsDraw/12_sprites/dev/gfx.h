7760d02f17ecc843086a93aca670c29b3fdec856
StevePro7 <steveprozune@hotmail.com>
2019-12-25 19:49:21 +0000

    no message

---
+++ GraphicsDraw/12_sprites/dev/gfx.h
@@ -0,0 +1,36 @@
+extern const unsigned char	adriana__palette__bin[];
+#define				adriana__palette__bin_size 16
+
+extern const unsigned char	adriana__tiles__psgcompr[];
+#define				adriana__tiles__psgcompr_size 62
+
+extern const unsigned char	font_tiles__palette__bin[];
+#define				font_tiles__palette__bin_size 16
+
+extern const unsigned char	font_tiles__tilemap__bin[];
+#define				font_tiles__tilemap__bin_size 128
+
+extern const unsigned char	font_tiles__tiles__psgcompr[];
+#define				font_tiles__tiles__psgcompr_size 663
+
+extern const unsigned char	skullnc__palette__bin[];
+#define				skullnc__palette__bin_size 16
+
+extern const unsigned char	skullnc__tiles__psgcompr[];
+#define				skullnc__tiles__psgcompr_size 54
+
+extern const unsigned char	sprites__palette__bin[];
+#define				sprites__palette__bin_size 16
+
+extern const unsigned char	sprites__tiles__psgcompr[];
+#define				sprites__tiles__psgcompr_size 998
+
+extern const unsigned char	tree_avoid__palette__bin[];
+#define				tree_avoid__palette__bin_size 16
+
+extern const unsigned char	tree_avoid__tilemap__bin[];
+#define				tree_avoid__tilemap__bin_size 8
+
+extern const unsigned char	tree_avoid__tiles__psgcompr[];
+#define				tree_avoid__tiles__psgcompr_size 82
+
