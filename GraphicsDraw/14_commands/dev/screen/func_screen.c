88df714140a95ebe3f3a54c57c8e8382626125b6
StevePro7 <steveprozune@hotmail.com>
2019-12-27 18:06:40 +0000

    no message

--- GraphicsDraw/14_commands/dev/screen/func_screen.c
+++ GraphicsDraw/14_commands/dev/screen/func_screen.c
@@ -3,16 +3,36 @@
 #include "..\engine\enemy_manager.h"
 #include "..\engine\font_manager.h"
 
+static unsigned char count;
+static unsigned char frame;
+static unsigned char timer;
+
 void screen_func_screen_load()
 {
 	engine_font_manager_draw_text( "HELLO FUNC SCREEN", 2, 1 );
 
 	engine_enemy_manager_init();
 	engine_enemy_manager_load();
+
+	count = 0;
+	frame = 0;
+	timer = 60;
+
+	engine_font_manager_draw_data( frame, 30, 0 );
 }
 
 void screen_func_screen_update( unsigned char *screen_type )
 {
 	engine_enemy_manager_draw();
+
+	count++;
+	if( count >= timer )
+	{
+		frame++;
+		engine_font_manager_draw_data( frame, 30, 0 );
+		//engine_enemy_manager_update();
+		count = 0;
+	}
+
 	*screen_type = screen_type_func;
 }
