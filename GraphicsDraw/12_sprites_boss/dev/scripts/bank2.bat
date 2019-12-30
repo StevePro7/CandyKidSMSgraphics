8a87a5e301411ab70db51d9f75a2a4fc343b687f
StevePro7 <steveprozune@hotmail.com>
2019-12-30 11:56:55 +0000

    no message

---
+++ GraphicsDraw/12_sprites_boss/dev/scripts/bank2.bat
@@ -0,0 +1,22 @@
+REM bank2	Generic sprite tiles
+@echo off
+
+cd ..
+cd banks
+cd bank2
+
+REM player
+
+bmp2tile.exe raw\boss32_adi01.bmp -savetiles "boss32_adi01 (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -exit
+
+cd ..
+folder2c bank2 bank2 2
+
+sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK2 bank2.c
+
+del *.asm > nul
+del *.lst > nul
+del *.sym > nul
+
+cd ..
+cd scripts
