37f23bac09522fb650ada0120b98207e3f10f6b0
StevePro7 <steveprozune@hotmail.com>
2019-12-30 21:43:49 +0000

    no message

--- GraphicsDraw/12_spritesAll/dev/engine/content_manager.c
+++ GraphicsDraw/12_spritesAll/dev/engine/content_manager.c
@@ -22,6 +22,12 @@ void engine_content_manager_load_tiles()
 void engine_content_manager_load_sprites()
 {
 	// Sprite tiles.
+
+	devkit_SMS_mapROMBank( boss32_adi01__tiles__psgcompr_bank );
+	devkit_SMS_loadPSGaidencompressedTiles( boss32_adi01__tiles__psgcompr, SPRITE_TILES );
+	devkit_SMS_loadSpritePalette( ( void * ) boss32_adi01__palette__bin );
+
+
 	//devkit_SMS_mapROMBank( boss64_adi01__tiles__psgcompr_bank );
 	//devkit_SMS_loadPSGaidencompressedTiles( boss64_adi01__tiles__psgcompr, SPRITE_TILES );
 	//devkit_SMS_loadSpritePalette( ( void * ) boss64_adi01__palette__bin );
@@ -35,9 +41,9 @@ void engine_content_manager_load_sprites()
 	devkit_SMS_loadPSGaidencompressedTiles( boss64_pro01__tiles__psgcompr, SPRITE_TILES );
 	devkit_SMS_loadSpritePalette( ( void * ) boss64_pro01__palette__bin );*/
 
-	devkit_SMS_mapROMBank( boss64_pro02__tiles__psgcompr_bank );
+	/*devkit_SMS_mapROMBank( boss64_pro02__tiles__psgcompr_bank );
 	devkit_SMS_loadPSGaidencompressedTiles( boss64_pro02__tiles__psgcompr, SPRITE_TILES );
-	devkit_SMS_loadSpritePalette( ( void * ) boss64_pro02__palette__bin );
+	devkit_SMS_loadSpritePalette( ( void * ) boss64_pro02__palette__bin );*/
 
 
 	/*devkit_SMS_mapROMBank( boss64_suz01__tiles__psgcompr_bank );
