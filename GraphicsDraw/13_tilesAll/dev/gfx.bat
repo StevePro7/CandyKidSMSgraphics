ea2b15543c8d769789920fd7a22c4fd9960518b0
StevePro7 <steveprozune@hotmail.com>
2020-01-01 15:27:14 +0000

    no message

--- GraphicsDraw/13_tilesAll/dev/gfx.bat
+++ GraphicsDraw/13_tilesAll/dev/gfx.bat
@@ -6,5 +6,8 @@ bmp2tile.exe raw\font_tiles.bmp -savetiles "font_tiles (tiles).psgcompr" -noremo
 bmp2tile.exe raw\apex_tiles.png -savetiles "apex_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 64 -savetilemap "apex_tiles (tilemap).bin" -savepalette "apex_tiles (palette).bin" -fullpalette -exit
 ::bmp2tile.exe raw\data_tiles.bmp -savetiles "data_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset  64 -savetilemap "data_tiles (tilemap).bin" -savepalette "data_tiles (palette).bin" -fullpalette -exit
 
+:: Sprites
+bmp2tile.exe raw\sprites.bmp -savetiles "sprites (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "sprites (palette).bin" -fullpalette  -spritepalette -exit
+
 cd ..
 cd dev
