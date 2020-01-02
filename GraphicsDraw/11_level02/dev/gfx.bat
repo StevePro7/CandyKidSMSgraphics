d58c4031a9450c06b423827dda27e1a72dbd234f
StevePro7 <steveprozune@hotmail.com>
2020-01-02 19:02:15 +0000

    no message

--- GraphicsDraw/11_level02/dev/gfx.bat
+++ GraphicsDraw/11_level02/dev/gfx.bat
@@ -3,12 +3,11 @@ cd gfx
 
 :: Tiles
 bmp2tile.exe raw\font_tiles.bmp -savetiles "font_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 0  -savetilemap "font_tiles (tilemap).bin" -savepalette "font_tiles (palette).bin" -fullpalette -exit
-bmp2tile.exe raw\apex_tiles.png -savetiles "apex_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 64 -savetilemap "apex_tiles (tilemap).bin" -savepalette "apex_tiles (palette).bin" -fullpalette -exit
+bmp2tile.exe raw\game_tiles.png -savetiles "game_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 64 -savetilemap "game_tiles (tilemap).bin" -savepalette "game_tiles (palette).bin" -fullpalette -exit
 
 :: Sprites
 :: https://github.com/maxim-zhao/bmp2tile
-bmp2tile.exe raw\sprites.bmp -savetiles "sprites (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "sprites (palette).bin" -fullpalette  -spritepalette -exit
-bmp2tile.exe raw\adriana.bmp -savetiles "adriana (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 64 -savepalette "adriana (palette).bin" -fullpalette  -spritepalette -exit
+bmp2tile.exe raw\sprites.png -savetiles "sprites (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "sprites (palette).bin" -fullpalette  -spritepalette -exit
 
 cd ..
 cd dev
