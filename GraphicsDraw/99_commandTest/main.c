a020a89ac4c38824131350013f1bf028ff12f22f
StevePro7 <steveprozune@hotmail.com>
2020-01-06 13:40:03 +0000

    no message

---
+++ GraphicsDraw/99_commandTest/main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+void main( void )
+{
+	struct light *light;
+
+	light = malloc( sizeof( *light ) );
+}
---
+++ GraphicsDraw/99_commandTest/main.h
@@ -0,0 +1,9 @@
+#ifndef __MAIN__
+#define __MAIN__
+
+#include "light.h"
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+#endif//__MAIN__
