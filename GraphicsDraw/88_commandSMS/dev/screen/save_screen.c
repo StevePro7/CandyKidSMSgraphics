25d6f08fe31bc289fdc31136d09a7cda730f1ba8
StevePro7 <steveprozune@hotmail.com>
2020-01-10 15:56:33 +0000

    no message

--- GraphicsDraw/88_commandSMS/dev/screen/save_screen.c
+++ GraphicsDraw/88_commandSMS/dev/screen/save_screen.c
@@ -1,10 +1,28 @@
 #include "save_screen.h"
 #include "..\engine\enum_manager.h"
 #include "..\engine\font_manager.h"
+#include "..\engine\input_manager.h"
+#include "..\engine\storage_manager.h"
+#include <stdbool.h>
 
 void screen_save_screen_load()
 {
-	engine_font_manager_draw_text( "SAVE SCREEN!!", 4, 4 );
+	bool test = isSaveGamePresent();
+	engine_font_manager_draw_text( "HELLO", 2, 0 );
+	engine_font_manager_draw_data( test, 2, 2 );
+	engine_font_manager_draw_text( "THERE", 20, 2 );
+	if( test )
+	{
+		engine_font_manager_draw_data( getPoints(), 12, 4 );
+		readSaveGame();
+		engine_font_manager_draw_data( getPoints(), 12, 5 );
+		incPoints();
+		engine_font_manager_draw_data( getPoints(), 12, 6 );
+	}
+
+	writeSaveGame();
+
+	engine_font_manager_draw_text( "SAVE SCREEN!!!!!!", 4, 4 );
 }
 
 void screen_save_screen_update( unsigned char *screen_type )
