6e416d005f150d098a9837d9bfeac3c0a85362bb
StevePro7 <steveprozune@hotmail.com>
2020-01-14 08:42:26 +0000

    no message

--- GraphicsDraw/89_commandSMS/dev/engine/actor_manager.c
+++ GraphicsDraw/89_commandSMS/dev/engine/actor_manager.c
@@ -1,7 +1,7 @@
 #include "actor_manager.h"
 #include "font_manager.h"
 
-static void print( char *text, unsigned int idx, unsigned char x, unsigned char y );
+static void print( char *text, unsigned int idx, unsigned intx );
 // Public method.
 void engine_actor_manager_update()
 {
@@ -11,18 +11,17 @@ void engine_actor_manager_update()
 void engine_actor_manager_exec_fire( unsigned int args )
 {
 	engine_font_manager_draw_text( "EXEC FIRE", 2, 12 );
+	engine_font_manager_draw_data( args, 17, 12 );
 }
 void engine_actor_manager_exec_jump( unsigned int args )
 {
 	engine_font_manager_draw_text( "EXEC JUMP", 2, 13 );
-	//engine_font_manager_draw_data( co->args1, 17, 14 );
-	//engine_font_manager_draw_data( co->args2, 27, 14 );
+	engine_font_manager_draw_data( args, 17, 13 );
 }
 void engine_actor_manager_exec_move( unsigned int args )
 {
 	engine_font_manager_draw_text( "EXEC MOVE", 2, 14 );
-	//engine_font_manager_draw_data( co->args1, 17, 14 );
-	//engine_font_manager_draw_data( co->args2, 27, 14 );
+	engine_font_manager_draw_data( args, 17, 14 );
 }
 
 // Undo commands.
@@ -30,18 +29,19 @@ void engine_actor_manager_undo_fire( unsigned int args )
 {
 	//engine_font_manager_draw_text( "UNDO FIRE", 2, 12 );
 	engine_font_manager_draw_text( "         ", 2, 12 );
+	args = 0;
 }
 void engine_actor_manager_undo_jump( unsigned int args )
 {
 	//engine_font_manager_draw_text( "UNDO JUMP", 2, 13 );
 	engine_font_manager_draw_text( "         ", 2, 13 );
+	args = 0;
 }
 void engine_actor_manager_undo_move( unsigned int args )
 {
 	//engine_font_manager_draw_text( "UNDO MOVE", 2, 14 );
 	engine_font_manager_draw_text( "         ", 2, 14 );
-	//engine_font_manager_draw_data( co->args1, 17, 18 );
-	//engine_font_manager_draw_data( co->args2, 27, 18 );
+	args = 0;
 }
 
 static void print( char *text, unsigned int idx, unsigned int args1 )
