10b14d6a57db9e27f0ad0dd60e3826b9babefdd0
StevePro7 <steveprozune@hotmail.com>
2020-01-14 16:10:31 +0000

    no message

--- GraphicsSave_BinaryFile/Program.cs
+++ GraphicsSave_BinaryFile/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.IO;
 
 namespace GraphicsSave_BinaryFile
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-		}
-	}
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            DisplayValues("output.ssm");
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void DisplayValues(string fileName)
+        {
+            float aspectRatio;
+
+            if(File.Exists(fileName))
+            {
+                using(var reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+                {
+                    aspectRatio = reader.ReadByte();
+                }
+
+
+            }
+        }
+    }
 }
