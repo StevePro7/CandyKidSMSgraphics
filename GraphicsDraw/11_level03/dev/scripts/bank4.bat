2b6fbb5decb0f18835c6cb16a337760a6598afd5
StevePro7 <steveprozune@hotmail.com>
2020-01-18 10:37:25 +0000

    no message

--- GraphicsDraw/11_level03/dev/scripts/bank4.bat
+++ GraphicsDraw/11_level03/dev/scripts/bank4.bat
@@ -2,7 +2,7 @@ REM bank2	Generic sprite tiles
 @echo off
 
 cd ..
-cd banks
+cd banksn
 cd bank4
 
 REM boss32
@@ -19,9 +19,8 @@ bmp2tile.exe raw\boss64_04.png -savetiles "boss64_04 (tiles).psgcompr" -noremove
 bmp2tile.exe raw\boss64_05.png -savetiles "boss64_05 (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -exit
 
 cd ..
-folder2c bank2 bank2 2
-
-sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK2 bank2.c
+folder2c bank4 bank2 4
+sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK4 bank4.c
 
 del *.asm > nul
 del *.lst > nul
