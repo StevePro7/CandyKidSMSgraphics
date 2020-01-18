d431ac87ef248f93e5acb858ed40eae44ba03014
StevePro7 <steveprozune@hotmail.com>
2020-01-18 11:19:26 +0000

    no message

--- GraphicsDraw/11_level03/dev/scripts/bank4.bat
+++ GraphicsDraw/11_level03/dev/scripts/bank4.bat
@@ -6,17 +6,19 @@ cd banks
 cd bank4
 
 REM boss32
-bmp2tile.exe raw\boss32_00.png -savetiles "boss32_00 (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -exit
-bmp2tile.exe raw\boss32_01.png -savetiles "boss32_01 (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -exit
-bmp2tile.exe raw\boss32_02.png -savetiles "boss32_02 (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -exit
+bmp2tile.exe raw\boss32_00.png -savetiles "boss32_00 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "boss32_00 (palette).bin" -fullpalette -spritepalette -exit
+bmp2tile.exe raw\boss32_01.png -savetiles "boss32_01 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "boss32_01 (palette).bin" -fullpalette -spritepalette -exit
+bmp2tile.exe raw\boss32_02.png -savetiles "boss32_02 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "boss32_02 (palette).bin" -fullpalette -spritepalette -exit
 
-REM boss64
-bmp2tile.exe raw\boss64_00.png -savetiles "boss64_00 (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -exit
-bmp2tile.exe raw\boss64_01.png -savetiles "boss64_01 (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -exit
-bmp2tile.exe raw\boss64_02.png -savetiles "boss64_02 (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -exit
-bmp2tile.exe raw\boss64_03.png -savetiles "boss64_03 (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -exit
-bmp2tile.exe raw\boss64_04.png -savetiles "boss64_04 (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -exit
-bmp2tile.exe raw\boss64_05.png -savetiles "boss64_05 (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -exit
+
+
+REM boss32
+bmp2tile.exe raw\boss64_00.png -savetiles "boss64_00 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "boss64_00 (palette).bin" -fullpalette -spritepalette -exit
+bmp2tile.exe raw\boss64_01.png -savetiles "boss64_01 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "boss64_01 (palette).bin" -fullpalette -spritepalette -exit
+bmp2tile.exe raw\boss64_02.png -savetiles "boss64_02 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "boss64_02 (palette).bin" -fullpalette -spritepalette -exit
+bmp2tile.exe raw\boss64_00.png -savetiles "boss64_03 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "boss64_03 (palette).bin" -fullpalette -spritepalette -exit
+bmp2tile.exe raw\boss64_01.png -savetiles "boss64_04 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "boss64_04 (palette).bin" -fullpalette -spritepalette -exit
+bmp2tile.exe raw\boss64_02.png -savetiles "boss64_05 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "boss64_05 (palette).bin" -fullpalette -spritepalette -exit
 
 cd ..
 folder2c bank4 bank4 4
