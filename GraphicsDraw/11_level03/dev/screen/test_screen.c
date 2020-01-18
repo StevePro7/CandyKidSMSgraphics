1ab580a31f5c24ba25640c0066f2b037ea9de092
StevePro7 <steveprozune@hotmail.com>
2020-01-18 20:13:51 +0000

    no message

--- GraphicsDraw/11_level03/dev/screen/test_screen.c
+++ GraphicsDraw/11_level03/dev/screen/test_screen.c
@@ -7,7 +7,9 @@
 void screen_test_screen_load()
 {
 	engine_boss_manager_load();
-	engine_font_manager_draw_text( "TEST SCREEN!!!!", 2, 2 );
+	engine_font_manager_draw_text( "TEST SCREEN....!!!!", 2, 2 );
+
+	engine_audio_manager_music_title();
 }
 
 void screen_test_screen_update( unsigned char *screen_type )
