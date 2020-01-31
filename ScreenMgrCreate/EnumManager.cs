3ffd7cdf93ded0ec3a469d1ea80bc56564f215f7
StevePro7 <steveprozune@hotmail.com>
2020-01-31 16:11:15 +0000

    no message

---
+++ ScreenMgrCreate/EnumManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace ScreenMgrCreate
+{
+	public class EnumManager
+	{
+		public void Build(string[] screens)
+		{
+			var lines = new List<string>
+			{
+				"#ifndef _ENUM_MANAGER_H_",
+				"#define _ENUM_MANAGER_H_",
+				"",
+
+				"typedef enum tag_enum_screen_type",
+				"{"
+			};
+
+			var index = 0;
+			foreach(var screen in screens)
+			{
+				lines.Add($"	screen_type_{screen} = {index++},");
+			}
+
+			lines.Add("");
+			lines.Add("}");
+
+			lines.Add("");
+			lines.Add("#endif//_ENUM_MANAGER_H_");
+
+			File.WriteAllLines("Managers/enum_manager.h", lines.ToArray());
+		}
+	}
+}
--- ScreenMgrCreate/Program.cs
+++ ScreenMgrCreate/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ScreenMgrCreate
 {
@@ -6,8 +7,24 @@ namespace ScreenMgrCreate
 	{
 		private static void Main(string[] args)
 		{
+			CreateDirectory("Managers");
+			CreateDirectory("Screens");
+
+			var screens = File.ReadAllLines("Levels.txt");
+			foreach(var screen in screens)
+			{
+
+			}
 
 			Console.WriteLine();
+		}
+
+		private static void CreateDirectory(string directory)
+		{
+			if(!Directory.Exists(directory))
+			{
+				Directory.CreateDirectory(directory);
+			}
 		}
 	}
 }
