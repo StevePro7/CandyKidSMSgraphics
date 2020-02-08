5076e592d64ed0763c63ca59c480d105cc8eec24
StevePro7 <steveprozune@hotmail.com>
2020-02-08 16:00:37 +0000

    no message

--- ROMbytesLeft/Program.cs
+++ ROMbytesLeft/Program.cs
@@ -9,9 +9,12 @@ namespace ROMbytesLeft
 			var fileManager = new FileManager();
 			fileManager.Build("output.sms");
 
-			Console.WriteLine("Index : " + fileManager.Index.ToString("X2"));
-			Console.WriteLine("Count : " + fileManager.Count);
-			Console.WriteLine("Total : " + fileManager.Total);
+			Console.WriteLine("32 KB : 32,768 bytes");
+			Console.WriteLine("Total : " + fileManager.Total.ToString("N0") + " bytes free");
+			Console.WriteLine("Count : " + fileManager.Count.ToString("N0") + " bytes ends" );
+			//Console.WriteLine("Diff  : " + (fileManager.Total - fileManager.Count).ToString("N0") + " bytes diff");
+			Console.WriteLine("Index : 0x" + fileManager.Index.ToString("X2")+ " bytes last" );
+
 			Console.WriteLine();
 		}
 
