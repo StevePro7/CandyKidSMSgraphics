19c2306004d25659de51a523abed67b33c73e93c
StevePro7 <steveprozune@hotmail.com>
2020-04-02 22:26:35 +0100

    no message

---
+++ LevelCompare/CompareManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LevelCompare
+{
+    public class CompareManager
+    {
+        public Boolean Process(string[] data1, string[] data2)
+        {
+            bool result = false;
+            if (data1.Length != data2.Length)
+            {
+                return result;
+            }
+
+            int count = data1.Length;
+            for (int index = 0; index < count; index++)
+            {
+                var line1 = data1[index];
+                var line2 = data2[index];
+                if (line1.Length != line2.Length)
+                {
+                    return result;
+                }
+
+                int chars = line1.Length;
+                for (int i = 0; i < chars; i++)
+                {
+                    char c1 = line1[i];
+                    char c2 = line2[i];
+                    if (c1 != c2)
+                    {
+                        return result;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LevelCompare/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LevelCompare
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var fileManager1 = new FileManager();
+			var fileManager2 = new FileManager();
+			var compareMgr = new CompareManager();
+
+			// bank2
+			for (int world = 1; world <= 10; world++)
+			{
+				for(int round = 1; round <= 10; round++)
+				{
+					var fileName1 = String.Format("level{0}{1}.txt", (world+ 0).ToString().PadLeft(2, '0'), round.ToString().PadLeft(2, '0'));
+					var fileName2 = String.Format("level{0}{1}.txt", (world+10).ToString().PadLeft(2, '0'), round.ToString().PadLeft(2, '0'));
+
+					fileManager1.Read("Levels/bank4/" + fileName1);
+					fileManager2.Read("Levels/bank5/" + fileName2);
+
+					var contents1 = fileManager1.Contents;
+					var contents2 = fileManager2.Contents;
+
+					var result = compareMgr.Process(contents1, contents2);
+					if (!result)
+					{
+						Console.WriteLine(fileName1 + " => " + fileName2);
+						Console.WriteLine();
+					}
+
+				}
+			}
+
+			// hiScore = 828,070 points
+			// Bonuses = 5k * 100 levels = 500,000
+			Console.WriteLine();
+		}
+	}
+}
