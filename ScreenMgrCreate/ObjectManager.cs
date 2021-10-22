ce7ad257103a75a43c4fc502ad03c48cb083ed11
StevePro7 <steveprozune@hotmail.com>
2021-10-22 09:50:57 +0100

    no message

---
+++ ScreenMgrCreate/ObjectManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ScreenMgrCreate
+{
+	public class ObjectManager
+	{
+		public void BuildObjects(string[] managers)
+		{
+			var manager = managers[0];
+			var header = $"_{manager.ToUpper()}_MANAGER_H_";
+
+			var lines = new List<string>
+			{
+				"#ifndef " + header,
+				"#define " + header,
+				"",
+				"",
+				"",
+
+				"##endif//" + header,
+			};
+
+		}
+
+		public void BuildObjectsX(string[] managers)
+		{
+			var lines = new List<string>
+			{
+				"#ifndef _ENUM_MANAGER_H_",
+				"#define _ENUM_MANAGER_H_",
+				"",
+
+				"typedef enum tag_enum_screen_type",
+				"{"
+			};
+
+			var index = 0;
+			foreach (var screen in screens)
+			{
+				lines.Add($"	screen_type_{screen.ToLower()} = {index++},");
+			}
+
+			lines.Add("");
+			lines.Add("} enum_screen_type;");
+
+			lines.Add("");
+			lines.Add("#endif//_ENUM_MANAGER_H_");
+
+			File.WriteAllLines("Managers/enum_manager.h", lines.ToArray());
+		}
+	}
+}
