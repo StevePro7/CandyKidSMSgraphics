e878895c14949e77e77a6a23f37a39e822649543
StevePro7 <steveprozune@hotmail.com>
2021-12-11 20:02:16 +0000

    no message

---
+++ AstroForceBuild/BuildManager.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ScreenMgrCreate
+{
+	public class BuildManager
+	{
+		public void Compile(string[] managersX, string[] screensX)
+		{
+			var managers = managersX.OrderBy(x => x).ToArray();
+			var screens = screensX.OrderBy(x => x).ToArray();
+			var lines = new List<string>();
+
+			var lines1 = new List<string>
+			{
+				"cd engine"
+			};
+			foreach (var manager in managers)
+			{
+				lines1.Add($"sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 {manager.ToLower()}_manager.c");
+			}
+			lines1.Add("cd ..");
+			lines1.Add("");
+			lines1.Add("");
+			lines1.Add("");
+
+
+			var lines2 = new List<string>
+			{
+				"cd object"
+			};
+			foreach (var manager in managers)
+			{
+				lines2.Add($"sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 {manager.ToLower()}_object.c");
+			}
+			lines2.Add("cd ..");
+			lines2.Add("");
+			lines2.Add("");
+			lines2.Add("");
+
+
+			var lines3 = new List<string>
+			{
+				"cd screen"
+			};
+			foreach(var screen in screens)
+			{
+				lines3.Add($"sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 {screen.ToLower()}_screen.c");
+			}
+			lines3.Add("cd ..");
+			lines3.Add("");
+			lines3.Add("");
+			lines3.Add("");
+
+			lines.AddRange(lines1);
+			lines.AddRange(lines2);
+			lines.AddRange(lines3);
+
+
+
+			var maxim = 5;
+			var index = 0;
+			var halve = 0;
+			var count = 0;
+			var line = "";
+
+
+			// Managers.
+			while(true)
+			{
+				var manager = managers[index];
+				line += $"engine/{manager.ToLower()}_manager.rel ";
+
+				count++;
+				if(count >= screens.Length)
+				{
+					line += "^";
+					lines.Add(line);
+					break;
+				}
+
+				index++;
+				halve++;
+				if(halve >= maxim)
+				{
+					line += "^";
+					lines.Add(line);
+					line = "";
+					halve = 0;
+				}
+			}
+
+			lines.Add("");
+			lines.Add("");
+			lines.Add("");
+
+			// Objects.
+			index = 0;
+			halve = 0;
+			count = 0;
+			line = "";
+			while (true)
+			{
+				var manager = managers[index];
+				line += $"object/{manager.ToLower()}_object.rel ";
+
+				count++;
+				if (count >= screens.Length)
+				{
+					line += "^";
+					lines.Add(line);
+					break;
+				}
+
+				index++;
+				halve++;
+				if (halve >= maxim)
+				{
+					line += "^";
+					lines.Add(line);
+					line = "";
+					halve = 0;
+				}
+			}
+
+			lines.Add("");
+			lines.Add("");
+			lines.Add("");
+
+			// Screens.
+			index = 0;
+			halve = 0;
+			count = 0;
+			line = "";
+			while (true)
+			{
+				var screen = screens[index];
+				line += $"screen/{screen.ToLower()}_screen.rel ";
+
+				count++;
+				if (count >= screens.Length)
+				{
+					line += "^";
+					lines.Add(line);
+					break;
+				}
+
+				index++;
+				halve++;
+				if (halve >= maxim)
+				{
+					line += "^";
+					lines.Add(line);
+					line = "";
+					halve = 0;
+				}
+			}
+
+			File.WriteAllLines("build.bat", lines.ToArray());
+		}
+
+	}
+}
---
+++ AstroForceBuild/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ScreenMgrCreate
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			CreateDirectory("Managers");
+			CreateDirectory("ManagersX");
+			CreateDirectory("Objects");
+			CreateDirectory("Screens");
+
+			var buildManager = new BuildManager();
+			var enumManager = new EnumManager();
+			var globalManager = new GlobalManager();
+			var objectManager = new ObjectManager();
+			var screenManager = new ScreenManager();
+
+			var screens = File.ReadAllLines("Levels.txt");
+			screens = GetScreens(screens);
+
+			var managers = File.ReadAllLines("engine.txt");
+			managers = GetManagers(managers);
+
+			objectManager.Build(managers);
+
+			buildManager.Compile(managers, screens);
+			enumManager.Build(screens);
+			globalManager.Build(screens);
+			screenManager.Build(screens);
+			screenManager.Construct(screens);
+			//screenManager.Switch(screens);
+
+			Console.WriteLine();
+		}
+
+		private static void CreateDirectory(string directory)
+		{
+			if(!Directory.Exists(directory))
+			{
+				Directory.CreateDirectory(directory);
+			}
+		}
+
+		private static string[] GetScreens(string[] screens)
+		{
+			IList<string> newScreens = new List<string>();
+			foreach (var screen in screens)
+			{
+				if (screen.StartsWith("--"))
+				{
+					continue;
+				}
+
+				newScreens.Add(screen);
+			}
+
+			return newScreens.ToArray();
+		}
+		private static string[] GetManagers(string[] managers)
+		{
+			IList<string> newManagers = new List<string>();
+			foreach (var manager in managers)
+			{
+				if (manager.StartsWith("--"))
+				{
+					continue;
+				}
+
+				newManagers.Add(manager);
+			}
+
+			return newManagers.ToArray();
+		}
+	}
+}
